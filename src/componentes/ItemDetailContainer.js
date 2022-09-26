7ac26e12d6c7ef75528abfeda80686c963c222b4
MelyIriarte <iriate.mela@gmail.com>
2022-09-26 14:42:35 -0300

    item collection

--- src/componentes/ItemDetailContainer.js
+++ src/componentes/ItemDetailContainer.js
@@ -1,26 +1,18 @@
 import React,{ useEffect, useState } from 'react';
 import ItemDetail from './ItemDetail';
-import Promesa from '../utils/Promesa';
-import BaseDatos from '../utils/BaseDatos.js'
+
 import { useParams } from 'react-router-dom';
-/* import { firestoreFetchOne } from '../utils/firestoreFetch';
-  */
+import { firestoreFetchOne } from '../utils/firestoreFetch';
+ 
 const ItemDetailContainer = () => {
 
  const [data, setData] = useState({});
  const {idItem} =useParams(); 
  useEffect(() => { 
-
-    Promesa(2000, BaseDatos.find(item => item.id === parseInt(idItem)))
-     .then(result => setData(result))
-     .catch(err => console.log(err))
-     
-   }, [idItem]); 
-/* 
    firestoreFetchOne(idItem)
    .then (result => setData(result))
    .catch(err => console.log(err))
-  },[]); */
+  },[idItem]);
  
     return (    
            <ItemDetail item={data}/>  
--- src/componentes/ItemListConteiner.js
+++ src/componentes/ItemListConteiner.js
@@ -1,25 +1,13 @@
 import React, { useState , useEffect } from "react";
-import Promesa from "../utils/Promesa.js"; 
-import BaseDatos from '../utils/BaseDatos' 
-import ItemList from "./ItemList.js";
 import {useParams} from 'react-router-dom';
-/* import {firestoreFetch} from '../utils/firestoreFetch' */
+import {firestoreFetch} from '../utils/firestoreFetch';
+import ItemList from "./ItemList.js";
 function ItemListConteiner() {
  
  const [data , setData] = useState([]); 
  const { idCategoria } = useParams();
-  useEffect( () => { 
-      if (idCategoria){
-      Promesa(2000, BaseDatos.filter(item => item.categoria === idCategoria ))
-      .then(datos => {  
-        setData(datos)})
-      .catch(err => console.log(err))
-     } 
-     else{
-      Promesa(2000, BaseDatos)
-      .then(datos => setData(datos))
-      .catch(err => console.log(err))  
-     } /* 
+ 
+ useEffect( () => { 
      firestoreFetch(idCategoria)
      .then(result => setData(result))
      .catch(err => console.log(err))
@@ -30,8 +18,7 @@ function ItemListConteiner() {
       setData([]);
     })
    }, []);
- */
-  },[idCategoria]);
+
     return(
       <ItemList data={data}/>
     );
